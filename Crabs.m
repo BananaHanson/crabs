ab92f2be4446ded6a395b2140e19f61c29bee7cf
BananaHanson <145809324+BananaHanson@users.noreply.github.com>
2023-09-22 14:08:10 -0700

    Add files via upload

---
+++ Crabs.m
@@ -0,0 +1,22 @@
+
+function retval = Crabs (input1, input2)
+
+% Crabs is a kids computer game where a fisherman, called the captain,
+% hunts for a very clever and powerful crab.
+% Draw the game map and initialize map dimensions.
+[mapHeight , mapWidth] = drawMap( "BGImage.png" );
+% Initialize captain location, heading and size
+xCapt = 2000;
+yCapt = 950;
+thetaCapt = -pi/2;
+sizeCapt = 50;
+% Draw the captain and initialize graphics handles
+%*********************************************************
+% Put your call to drawCapt() here ..... You must give drawCapt its
+% input and output arguments.
+
+captainGraphics = drawCapt (xCapt , yCapt , thetaCapt , sizeCapt)
+
+%*******************************************************
+
+endfunction
